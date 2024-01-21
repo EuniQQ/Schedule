e0e7ad3f0032e49534157a251a67093401eddb29
Eunice <eunice911qq@gmail.com>
2024-01-21 22:46:50 +0800

    feat : 新增側邊欄目錄

--- resources/views/content/home.blade.php
+++ resources/views/content/home.blade.php
@@ -5,22 +5,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="{{asset('css/home.css')}}">
+    <link rel="stylesheet" href="{{asset('css/menu.css')}}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <style>
     </style>
-
 </head>
 
 <title>首頁</title>
-</head>
+
 
 <body>
     <div class="container">
+        @include('layouts.menu')
         <header class="row d-flex">
-            <i class="fa-solid fa-bars" style="color: #616161;"></i>
+            <button class="btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBackdrop" aria-controls="offcanvasWithBackdrop" style="text-align:left">
+                <i class="fa-solid fa-bars" style="color: #616161;"></i>
+            </button>
+
             <i class="fa-solid fa-gear" style="color: #616161;"></i>
         </header>
+
 
         <div class="row d-flex content">
             <!-- 文字區 -->
@@ -49,7 +55,15 @@
 
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    </script>
+    <script>
+        $(document).on("click", ".closeMenu", function(e) {
+            let menu = document.querySelector(".closeMenu");
+            menu.style.display = "none";
+        })
+    </script>
+    <script src="{{ asset('resources/js/menu.js') }}"></script>
 </body>
 
 </html>
---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,24 @@
+<div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasWithBackdrop" aria-labelledby="offcanvasWithBackdropLabel">
+    <div class="offcanvas-header">
+        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close">
+    </div>
+    <div class="offcanvas-body">
+        <div class="row menu ">
+            </button>
+
+            <p><a href="{{ route('home') }}">HOME</a></p>
+            <p class="type">日&nbsp;&nbsp;誌</p>
+            <p>·&nbsp;當月月曆</p>
+            <p>·&nbsp;每月事紀</p>
+            <p>·&nbsp;每日記事</p>
+            <p class="type">記&nbsp;&nbsp;帳</p>
+            <p>·&nbsp;收入</p>
+            <p>·&nbsp;支出</p>
+            <p>·&nbsp;年度結餘</p>
+            <p class="type">照&nbsp;&nbsp;片</p>
+            <br><br>
+            <p>設&nbsp;&nbsp;定</p>
+            <p class="logout">登&nbsp;&nbsp;出</p>
+        </div>
+    </div>
+</div>
