da6d320e4ac0a16b632e67a1c41ca05af900995c
Eunice <eunice911qq@gmail.com>
2024-02-16 23:42:50 +0800

    feat : 建立通用函式檔

---
+++ public/js/module.js
@@ -0,0 +1,41 @@
+
+/**
+ * 取得apiToken
+ */
+function getApiToken() {
+    let issetApiToken = document.cookie.indexOf("api_token");
+    let apiToken = issetApiToken > 0 ? getCookie('api_token') : null;
+
+    return apiToken
+}
+
+
+/**
+ * 取得特定cookie值
+ */
+function getCookie(name) {
+    const value = "; " + document.cookie;
+    const parts = value.split("; " + name + "=");
+    if (parts.length == 2) {
+        return parts.pop().split(";").shift();
+    }
+}
+
+
+
+/**
+ * 上傳img同時預覽、可刪除
+ */
+function previewSelect(event) {
+    const inputId = event.target.id;
+    const input = document.getElementById(inputId);
+    const file = input.files[0];
+    const imgSet = input.parentNode.nextElementSibling;
+    const imgPre = imgSet.querySelector('img');
+
+    if (file) {
+        imgPre.src = URL.createObjectURL(file);
+    } else {
+        imgPre.src = "";
+    }
+}
