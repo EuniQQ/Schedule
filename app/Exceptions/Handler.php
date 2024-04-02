99c7f385fab8817dbe954ca09c38786c88244912
Eunice <eunice911qq@gmail.com>
2024-04-02 10:47:08 +0800

    fix : 修正登入後顯示419問題

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Session\TokenMismatchException;
 
 class Handler extends ExceptionHandler
 {
@@ -26,5 +27,22 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+
+    /**
+     * 登入時若出現CSRF TOKEN錯誤的處理方法
+     */
+    public function render($request, Throwable $exception)
+    {
+        if (!$request->ajax() && ($exception instanceof TokenMismatchException)) {
+            return redirect()
+                ->back()
+                ->withErrors('表單已過期，請重新提交')
+                ->withInput($request->input());
+        }
+
+        // 若是AJAX請求或不是CSRF錯誤，則按父類預設方法處理
+        return parent::render($request, $exception);
     }
 }
