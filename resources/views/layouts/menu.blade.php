2e1186d05128ecb82124ac9931be31596213b293
Eunice <eunice911qq@gmail.com>
2024-04-08 23:09:56 +0800

    fix : 修補判斷是否開放menu設定選項bug

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -28,7 +28,7 @@
         <div>·&nbsp;年度結餘</div>
         <div class="type"></div>
         <div>
-            @if(auth()->user()->id === 1 )
+            @if(Auth::check() && auth()->id === 1 )
             <a href="{{ route('profile.edit') }}" style="pointer-events: none">設&nbsp;&nbsp;定(暫不開放)</a>
             @else
             <a href="{{ route('profile.edit') }}">設&nbsp;&nbsp;定</a>
